f1a1a04851ffd7fe527005d94b48852b0393bc81
xiaooodd <63233120+xiaooodd@users.noreply.github.com>
2021-06-07 12:12:43 +0800

    add test multiply

--- test_calculator.py
+++ test_calculator.py
@@ -9,3 +9,6 @@ class TestCalculatorApp:
 
     def test_subtract(self):
         assert 5 == calculator.subtract(10, 5)
+
+    def test_multiply(self):
+        assert 4 == calculator.multiply(2, 2)
